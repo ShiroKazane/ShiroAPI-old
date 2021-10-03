05f0215110dcf2241e318c162a816b9bffd542a2
Azusa <49191595+AzusaAi@users.noreply.github.com>
2021-10-03 09:58:16 +0000

    Add some Neko & Baka

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const fs = require('fs');
 const bodyParser = require('body-parser');
 const favicon = require('serve-favicon');
+const multer = require('multer');
 const PORT = process.env.PORT || 8080;
 
 app.set('view engine', 'ejs');
@@ -11,14 +12,45 @@ app.use(favicon(__dirname + '/assets/favicon.ico'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
+app.use('/images', express.static('private'));
+
+// Neko
+
+let nekoStorage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './public/neko')
+  },
+  filename: function (req, file, cb) {
+    let files = fs.readdirSync('./public/neko');
+    cb(null, 'neko_' + ++files.length + path.extname(file.originalname))
+  }
+})
+let nekoUpload = multer({ storage: nekoStorage })
+
+// ------------------------------------------------------------------------------
+
+// Baka
+
+let bakaStorage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './public/baka')
+  },
+  filename: function (req, file, cb) {
+    let files = fs.readdirSync('./public/baka');
+    cb(null, 'baka_' + ++files.length + path.extname(file.originalname))
+  }
+})
+let bakaUpload = multer({ storage: bakaStorage })
+
+// ------------------------------------------------------------------------------
 
 app.get('/', (req, res) => {
-  res.send('Nothing.')
+  res.render('index');
 })
 
 app.get('/api', (req, res) => {
   const list = fs.readdirSync('./public');
-  res.json(list)
+  res.json(list);
 })
 
 app.get('/api/:id', (req, res) => {
@@ -39,6 +71,29 @@ app.get('/api/:id', (req, res) => {
     return res.status(404).render('404');
   }
 });
+
+app.get('/upload/:id', (req, res) => {
+  let param = './public/' + req.params.id;
+  if (fs.existsSync(param)) {
+    res.render('upload', {
+      id: req.params.id
+    });
+  } else {
+    return res.status(404).render('404');
+  }
+})
+
+app.post('/upload/neko', nekoUpload.single('upload'), (req, res) => {
+  res.render('upload', {
+    id: 'neko'
+  });
+})
+
+app.post('/upload/baka', bakaUpload.single('upload'), (req, res) => {
+  res.render('upload', {
+    id: 'baka'
+  });
+})
 
 app.all('*', function (req, res, next) {
   res.status(404).render('404');
